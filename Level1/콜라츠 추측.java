0d1c007ada0df42649f58d8e20ecaf974a367558
KimJoongHyeok <83209006+KimJoongHyeok@users.noreply.github.com>
2021-06-24 00:08:21 +0900

    Create 콜라츠 추측.java

---
+++ Level1/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long num1 = num;
+            while(num1 !=1 ){
+                if(num1 % 2 == 0){
+                    num1 /= 2;
+                }
+                else{
+                    num1 = 3 * num1 + 1;
+                }
+                answer++;
+                if(answer>500){
+                    return -1;
+                }
+            }
+        return answer;
+    }
+}
