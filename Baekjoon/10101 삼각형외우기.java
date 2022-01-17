e7d6a7be2cd91ff7f8d566a944b55ba567f93fa1
KimJoongHyeok <moon960427@gmail.com>
2022-01-17 19:58:48 +0900

    Beakjoon_Java

---
+++ Baekjoon/10101 삼각형외우기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        String result = search(a,b,c);
+        System.out.println(result);
+    }
+    static String search(int a,int b,int c){
+        if(a+b+c == 180){
+            if(a != b && a != c && b != c){
+                return "Scalene";
+            }
+            else if(a == 60 && b == 60 && c == 60){
+                return "Equilateral";
+            }
+            return "Isosceles";
+        }
+        return "Error";
+    }
+}
