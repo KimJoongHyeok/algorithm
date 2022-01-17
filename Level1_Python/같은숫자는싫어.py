41293d6ebca5c863177b50d0d6de50ceb46922e9
KimJoongHyeok <moon960427@gmail.com>
2022-01-17 19:58:05 +0900

    Programmers_Level1_Java

--- Level1_Python/같은숫자는싫어.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    answer = []
-    #answer = list(set(arr))
-    arr.append(-1)
-    for i in range(len(arr)-1):
-        if(arr[i] != arr[i+1]):
-            answer.append(arr[i])
-    return answer
