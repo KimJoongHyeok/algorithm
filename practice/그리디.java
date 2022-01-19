4687dda6b5ee9bd4686a76857c2a94eaf68f3f33
KimJoongHyeok <moon960427@gmail.com>
2022-01-19 19:26:55 +0900

    그리디 기본 구현

---
+++ practice/그리디.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class 그리디 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    int result = 0;
+
+    while(true){
+      int target = (n/k)*k;
+      result += (n-target);
+      n = target;
+      if(n < k ) break;
+      n /= k;
+      result += 1;
+    }
+
+    result += n-1;
+    System.out.println(n);
+    System.out.println(result);
+  }  
+}
