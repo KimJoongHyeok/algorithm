f24d34613dd1263716055537b7a888e07fb2a076
KimJoongHyeok <moon960427@gmail.com>
2022-02-04 11:53:22 +0900

    문자열_단어의 개수

---
+++ Baekjoon/1152 단어의 개수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String line = sc.nextLine();
+
+    int count = 0;
+    
+    for(int i = 0;i<line.length();i++){
+      if(line.charAt(i) == ' '){
+        count++;
+      }
+    }
+    if(line.charAt(0) != ' ' && line.charAt(line.length()-1) != ' '){
+      count = count +1;
+    }
+    if(line.charAt(0) == ' ' && line.charAt(line.length()-1) == ' '){
+      count = count -1;
+    }
+    System.out.print(count);
+  }
+}
