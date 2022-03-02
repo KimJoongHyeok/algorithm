04e04925e0ab897e20ac41d72e3763b017a95e49
KimJoongHyeok <moon960427@gmail.com>
2022-03-02 14:46:15 +0900

    BOJ N&M 중복조합

--- Baekjoon/BOJ15657.java
+++ Baekjoon/BOJ15657.java
@@ -3,6 +3,7 @@ package com.Permutation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ15657 {
@@ -19,30 +20,41 @@ public class BOJ15657 {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
+        boolean[] visited = new boolean[arr.length];
+        comb(arr, visited, 0, 0, M); //일반조합
+
         int[] output = new int[M];
-        comb(arr, output, 0, 0, M);
+        //comb2(arr, output, 0, 0, M);
         System.out.println(sb);
     }
 
-    private static void comb(int[] arr, int[] output, int index, int cnt, int m) {
-        if (cnt == m) {
-            for (int i = 0; i < output.length; i++) {
-                sb.append(output[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
+    //일반 조합
+    private static void comb(int[] arr, boolean[] visited, int index, int cnt, int M) {
+        if (cnt == M) {
+
         }
         for (int i = index; i < arr.length; i++) {
-            if (index == 0) {
-                output[index] = arr[i];
-                comb(arr, output, i, cnt + 1, m);
-            }
-            if (index != 0 && output[index] <= arr[i]) {
-                output[index] = arr[i];
-                comb(arr, output, i, cnt + 1, m);
-            }else{
-                return;
-            }
+            visited[i] = true;
+            comb(arr, visited, index + 1, cnt + 1, M - 1);
+
+            visited[i] = false;
+            comb(arr, visited, index + 1, cnt + 1, M);
         }
     }
+    //중복조합
+//    private static void comb2(int[] arr, int[] output, int index, int cnt, int M) {
+//        if (cnt == M) {
+//            for (int i = 0; i < output.length; i++) {
+//                sb.append(output[i]).append(" ");
+//            }
+//            sb.append("\n");
+//            return;
+//        }
+//        for (int i = index; i < arr.length; i++) {
+//            output[cnt] = arr[i];
+//            comb2(arr, output, index, cnt + 1, M);
+//        }
+//    }
 }
+
