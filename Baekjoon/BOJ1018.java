10c2bd08e6f226f7c5e51f12e810ff34fcb873bd
KimJoongHyeok <moon960427@gmail.com>
2022-03-11 12:42:19 +0900

    브루트포스 체스판 다시 칠하기

--- Baekjoon/BOJ1018.java
+++ Baekjoon/BOJ1018.java
@@ -1,37 +1,77 @@
 package com.BruteForce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BOJ1018 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+    static char[][] w_first = {
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+    };
+    static char[][] b_first = {
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+    };
+    static int w_answer = 0;
+    static int b_answer = 0;
+    static int n, m;
+    static char[][] arr;
 
-        //arr배열에 입력값 넣기
-        Character [][]arr = new Character[m][n];
-        for(int i = 0;i<m;i++){
-            String chess = sc.next();
-            for(int j = 0;j<n;j++){
-                arr[i][j] = chess.charAt(j);
-                System.out.print(arr[i][j]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new char[n][m];
+
+        //배열 입력받기
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = s.charAt(j);
             }
-            System.out.println();
         }
 
-        ArrayList<Integer>list = new ArrayList<Integer>();
-        int count = 0;
-        for(int i = 0;i<n-8;i++){
-            for(int j = 0;j<m-8;j++){
-                for(int k = 1;k<8;k++){
-                    for(int l = 1;l<8;l++){
-                        if(arr[k-1][l-1] == arr[k][l]){
-                            count++;
-                        }
-                    }
-                }
+        int answer = m*n;
+        for (int i = 0; i < n - 7; i++) { //행 시작 인덱스
+            for (int j = 0; j < m - 7; j++) { // 열 시작 인덱스
+                find(i, j);
+                answer = Math.min(answer, Math.min(w_answer, b_answer));
+                w_answer = 0;
+                b_answer = 0;
             }
+        }
+        System.out.println(answer);
+    }
+
+    static void find(int i, int j) {
+        int a = 0;
+        for (int x = i; x < i + 8; x++) {
+            int b = 0;
+            for (int y = j; y < j + 8; y++) {
+                if (arr[x][y] != w_first[a][b]) w_answer++;
+                if (arr[x][y] != b_first[a][b]) b_answer++;
+                b++;
+            }
+            a++;
         }
     }
+
+
 }
