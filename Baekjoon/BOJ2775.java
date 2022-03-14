d7e52a70c19bcd18da8958bd069465c65d88924f
KimJoongHyeok <moon960427@gmail.com>
2022-03-15 01:46:33 +0900

    기본수학1 부녀회장이 될테야

---
+++ Baekjoon/BOJ2775.java
@@ -0,0 +1,37 @@
+package com.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[][] arr = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            arr[0][i] = i;
+            arr[i][0] = i;
+            arr[i][1] = 1;
+        }
+
+        for (int i = 1; i <= 14; i++) {
+            for (int j = 2; j <= 14; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            }
+        }
+
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(arr[k][n]);
+        }
+    }
+}
